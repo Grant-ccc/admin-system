66be7f02b60e43d4f078e540c4aa2ede74a9d9b1
Grant-ccc <24774762722@qq.com>
2026-03-23 08:40:46 +0800

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/admin-system',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
